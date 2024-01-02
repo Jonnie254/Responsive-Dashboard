fa849e6da5e9ffb3b6ea033b4d271cfab853bbec
jonnie254 <johnkamanu254@proton.me>
2024-01-02 22:18:54 +0300

    updated js file

---
+++ orders.js
@@ -0,0 +1,19 @@
+const Orders = [
+{
+    productName:'Javascript Tutorial',
+    productNumber:'85743',
+    paymentStatus:'Due',
+    status:'Pending'
+},
+{
+    productName:'CSS Full Course',
+    productNumber:'857413',
+    paymentStatus:'Refunded',
+    status:'Declined'
+},
+{
+    productName:'Flex-box Tutorial',
+    productNumber:'857431',
+    paymentStatus:'Paid',
+    status:'Active'}
+]
